494858ef8c9119711ffec072960db2152bbbb195
Jack Lenox <jack.lenox@gmail.com>
2014-09-25 09:01:15 +0100

    Reorganise js a bit, enable admin bar links

--- js/rest-api-theme.js
+++ js/rest-api-theme.js
@@ -3,24 +3,6 @@ jQuery(document).ready(function($) {
 	var app = app || {};
 	var templateUrl = script_data.templateUrl;
 
-	$(document).delegate("a", "click", function(evt) {
-		// Get the anchor href and protcol
-		var href = $(this).attr("href");
-		var protocol = this.protocol + "//";
-		var pathname = this.pathname;
-
-		// Ensure the protocol is not part of URL, meaning its relative.
-		// Stop the event bubbling to ensure the link will not cause a page refresh.
-		if (href.slice(protocol.length) !== protocol) {
-			evt.preventDefault();
-
-			// Note by using Backbone.history.navigate, router events will not be
-			// triggered.  If this is a problem, change this to navigate on your
-			// router.
-			app.router.navigate( pathname, true );
-		}
-	});
-
 	var Post = Backbone.Model.extend({
 		defaults: {
 			'the-loop': ''
@@ -98,6 +80,23 @@ jQuery(document).ready(function($) {
 
 	Backbone.history.start({
 		pushState: true,
+	});
+
+	// Handle all clicks
+	$(document).delegate("a:not(.ab-item)", "click", function(evt) {
+		// Get the anchor href and protcol
+		var href = $(this).attr("href");
+		var protocol = this.protocol + "//";
+		var pathname = this.pathname;
+
+		// Ensure the protocol is not part of URL, meaning its relative.
+		// Stop the event bubbling to ensure the link will not cause a page refresh.
+		if (href.slice(protocol.length) !== protocol) {
+			evt.preventDefault();
+
+			// Instruct the navigator to route to the pathname
+			app.router.navigate( pathname, true );
+		}
 	});
 
 });
